78a812fa32821fa53f5851b1b712068bc2ea6810
Archana <archunadakattin@gmail.com>
2026-08-22 10:49:53 +0530

    Test CodeSense

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="collapsed"
 )
-
+# CodeSense testing
 
 # =========================================================
 # CUSTOM CSS
